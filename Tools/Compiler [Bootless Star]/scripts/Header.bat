00f3be37d203a7ece382ae2872fb9f3ae9131525
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2016-03-10 15:53:04 -0600

    Revised comments
    We're no longer depending on Subversion anymore.
    Revised project's official name.

--- Tools/Compiler [Bootless Star]/scripts/Header.bat
+++ Tools/Compiler [Bootless Star]/scripts/Header.bat
@@ -1,13 +1,13 @@
 REM Created by Nicholas "Tiger" Gautier
 REM Contact: Nicholas.Gautier.Tiger@GMail.com
 REM Website: https://github.com/SibTiger/
-REM Details: This program allows the user to compile a developmental or a release version of the Morgenstern project directly from the subversion local working copy, this program is essentially a complexed makefile written in Batch.
+REM Details: This program allows the user to compile a developmental or a release version of the Morgenstern project directly from the project's local working copy, this program is essentially a complexed makefile written in Batch.
 REM Prerequisites:
 REM     Bootless Star 1.5 or later
 REM         http://forum.drdteam.org/viewtopic.php?p=53914#p53914
 REM     7Zip 9.20 preferred
 REM         http://7-zip.org/
-REM     Morgenstern resources [SVN Local Working Copy]
+REM     TGRDM3 [Morgenstern] resources [Local Working Copy]
 REM ==========================================
 
 
