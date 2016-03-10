59a70b1d8e7f64ddb24ce9bdf8a15a4af601faa3
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2016-03-10 15:59:11 -0600

    Bootless Star 1.5.4 is required; Git Support needed
    This compiler must use Bootless Star version 1.5.4; this foundation has
    Git functionality which is what this compiler needs.

--- Tools/Compiler [Bootless Star]/scripts/Header.bat
+++ Tools/Compiler [Bootless Star]/scripts/Header.bat
@@ -3,7 +3,7 @@ REM Contact: Nicholas.Gautier.Tiger@GMail.com
 REM Website: https://github.com/SibTiger/
 REM Details: This program allows the user to compile a developmental or a release version of the Morgenstern project directly from the project's local working copy, this program is essentially a complexed makefile written in Batch.
 REM Prerequisites:
-REM     Bootless Star 1.5 or later
+REM     Bootless Star 1.5.4 or later
 REM         http://forum.drdteam.org/viewtopic.php?p=53914#p53914
 REM     7Zip 9.20 or later
 REM         http://7-zip.org/
