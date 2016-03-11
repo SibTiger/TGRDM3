cf8b8c0f933ffd99005514445a1a79b386401d8d
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2016-03-11 06:50:07 -0600

    [WIP] Thrashed superfluous code
    Expunged code that will no longer be used anymore.

--- Tools/Compiler [Bootless Star]/scripts/CompileProject_SubversionControl.bat
+++ Tools/Compiler [Bootless Star]/scripts/CompileProject_SubversionControl.bat
@@ -67,21 +67,6 @@ EXIT /B 0
 
 
 REM # =============================================================================================
-REM # Documentation: If incase the cached revision ID that is stored on the filesystem matches with the new value, then do NOT write a new [but duplicated] file.  This will prevent wear-and-tear the platter [for SSD killed cell blocks] and repetitive tasks.
-REM # =============================================================================================
-:CompileProject_FetchSVNRevisionID_CheckUpdateCachedRevision
-IF %SVNRevisionOld% EQU %SVNRevisionNew% (
-    CALL :CompileProject_Display_IncomingTaskSubLevelMSG "The revision difference between the newly stored revision matches with the previously stored revision, there is no new %ProjectName% updates."
-    SET SVNRevisionUpdateCachedRevisionID=False
-) ELSE (
-    CALL :CompileProject_Display_IncomingTaskSubLevelMSG "The revision difference indicates that there is %ProjectName% updates."
-    SET SVNRevisionUpdateCachedRevisionID=True
-)
-GOTO :EOF
-
-
-
-REM # =============================================================================================
 REM # Documentation: Setup the field and find the current revision number.
 REM # =============================================================================================
 :CompileProject_FetchSVNRevisionID_TaskNewRevision
@@ -96,70 +81,12 @@ EXIT /B %ERRORLEVEL%
 
 
 REM # =============================================================================================
-REM # Documentation: Load the previous saved revision ID to a variable.
-REM # =============================================================================================
-:CompileProject_FetchSVNRevisionID_OldRevision
-IF EXIST "%LocalDirectory.MainRoot%\SVNVersion.txt" (
-    CALL :CompileProject_Display_IncomingTaskSubLevelMSG "Caching previously saved revision number"
-    SET /P SVNRevisionOld=< "%LocalDirectory.MainRoot%\SVNVersion.txt"
-) ELSE (
-    CALL :CompileProject_Display_IncomingTaskSubLevelMSG "Could not find previously saved revision number, using 'Zero' instead"
-    SET SVNRevisionOld=0
-)
-GOTO :EOF
-
-
-
-REM # =============================================================================================
 REM # Documentation: Fetch the working copy revision number and store it in primary memory.
 REM # =============================================================================================
 :CompileProject_FetchSVNRevisionID_NewRevision
 CALL :CompileProject_Display_IncomingTaskSubLevel "Capturing the local Working Copy's current revision"
 FOR /F %%a IN ('SVNVERSION "%UserConfig.DirProjectWorkingCopy%"') DO SET SVNRevisionNew=%%a
 EXIT /B %ERRORLEVEL%
-
-
-
-REM # =============================================================================================
-REM # Documentation: It is possible that if the working copy is modified, the SVN Revision ID may contain non-integers.  For example: 1715M. This is a very special variable manipulator.  The idea is to remove any non-integer from the last line of the SVN Version number.
-REM #  NOTE: Mixed Revisions is NOT supported within this check, that'll require a much more complicated procedure.
-REM # =============================================================================================
-:CompileProject_FetchSVNRevisionID_CleanupResult
-CALL :CompileProject_Display_IncomingTaskSubLevel "Thrashing non-integer characters from the newly stored revision number"
-REM If the working copy is Modified, the SVN version fetched will be reported as 1715M.  Remove the Modified signature character from the variable.
-SET SVNRevisionNew=%SVNRevisionNew:M=%
-REM ----
-REM If the working copy is Switched, the SVN version fetched will be reported as 1715S.  Remove the Switch signature character from the variable.
-SET SVNRevisionNew=%SVNRevisionNew:S=%
-REM ----
-REM If the working copy is Partial, the SVN version fetched will be reported as 1715P.  Remove the Partial signature character from the variable.
-SET SVNRevisionNew=%SVNRevisionNew:P=%
-GOTO :EOF
-
-
-
-REM # =============================================================================================
-REM # Documentation: Calculate the difference between the new revision ID against the previously saved revision ID, and store the value in memory for further computation.
-REM # =============================================================================================
-:CompileProject_FetchSVNRevisionID_Calculate
-CALL :CompileProject_Display_IncomingTaskSubLevel "Calculating the difference from the current revision number and the previous revision"
-SET /A SVNRevisionRange=%SVNRevisionNew%-%SVNRevisionOld%
-GOTO :EOF
-
-
-
-REM # =============================================================================================
-REM # Documentation: Calculate the range and determine if it exceeds the HardLimit set.  If the calculated value is over the HardLimit range, then merely force the hard-limit to be the changelog limit.
-REM # =============================================================================================
-:CompileProject_FetchSVNRevisionID_CalculateRange
-CALL :CompileProject_Display_IncomingTaskSubLevel "Calculating the difference range"
-REM We need to make sure that the value we have from 'CompileProject_FetchSVNRevisionID_Calculate' does not exceed the hard limit variable 'SVNRevisionRangeHardLimit'
-IF %SVNRevisionRange% GTR %SVNRevisionRangeHardLimit% (
-    SET SVNRevisionRange=%SVNRevisionRangeHardLimit%
-) ELSE (
-    IF %SVNRevisionRange% LEQ 0 SET SVNRevisionRange=%SVNRevisionRangeHardLimit%
-)
-GOTO :EOF
 
 
 
@@ -205,55 +132,6 @@ SVN log -l %SVNRevisionRange% -r HEAD:1 "%UserConfig.DirProjectWorkingCopy%" 1> 
 REM Error Check
 CALL :CompileProject_SVNErrorCheck %ERRORLEVEL%
 EXIT /B %ERRORLEVEL%
-
-
-
-REM # =============================================================================================
-REM # Documentation: Generate a changelog history in an XML file.
-REM # =============================================================================================
-:CompileProject_FetchSVNRevisionHistoryXML
-IF %UserConfig.SVNAllowFetchRevisionLogXML% EQU False EXIT /B 0
-CALL :CompileProject_Display_IncomingTaskSubLevel "Fetching an advanced revision change log [xml formatting]"
-REM Fetch the log
-SET TaskCaller_CallLong=SVN log --xml -v -l %SVNRevisionRange%  -r HEAD:1 "%UserConfig.DirProjectWorkingCopy%" 1> "%LocalDirectory.Temp%\%FileName%\Changelog.xml"
-SET TaskCaller_NiceProgramName=Subversion
-SVN log --xml -v -l %SVNRevisionRange%  -r HEAD:1 "%UserConfig.DirProjectWorkingCopy%" 1> "%LocalDirectory.Temp%\%FileName%\Changelog.xml"
-REM Error Check
-CALL :CompileProject_SVNErrorCheck %ERRORLEVEL%
-EXIT /B %ERRORLEVEL%
-
-
-
-REM -------------------------------
-REM -------------------------------
-REM Update the cached revision number in the secondary storage
-
-
-REM # =============================================================================================
-REM # Documentation: Store the newly SVN revision ID in the secondary storage.  This will be used next time when compiling the project.
-REM # =============================================================================================
-:CompileProject_SVNUpdateCachedRevision
-REM ----
-REM Run this function?
-IF %Detect_SVN% EQU False EXIT /B 0
-IF %UserConfig.SVNMasterControl% EQU False EXIT /B 0
-REM ----
-REM This variable is used to describe the drivers main purpose and present the value in the log files.
-SET "DriversNiceTask=Updating cached revision ID"
-CALL :CompileProject_Display_IncomingTask "%DriversNiceTask%"
-CALL :CompileProject_FetchSVNRevisionID_UpdateCachedValue
-CALL :CompileProject_DriverLogFooter "%DriversNiceTask%"
-EXIT /B 0
-
-
-
-REM # =============================================================================================
-REM # Documentation: Save the information to the specified file for future use.
-REM # =============================================================================================
-:CompileProject_FetchSVNRevisionID_UpdateCachedValue
-CALL :CompileProject_Display_IncomingTaskSubLevel "Saving the revision [%SVNRevisionNew%] ID for future use"
-ECHO %SVNRevisionNew% 1> "%LocalDirectory.MainRoot%\SVNVersion.txt"
-GOTO :EOF
 
 
 
