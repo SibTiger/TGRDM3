abeb1aa37abebe5f902cd299f680c3271c2c0b7a
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2016-03-12 11:50:13 -0600

    Updated prerequisites
    Added Git VC CUI toolset.
    Added a website to the repo for the TGRDM3 project; incase if
    individuals need it - despite its redundancies, but who knows of the
    scenarios.

--- Tools/Compiler [Bootless Star]/scripts/Header.bat
+++ Tools/Compiler [Bootless Star]/scripts/Header.bat
@@ -5,9 +5,12 @@ REM Details: This program allows the user to compile a developmental or a releas
 REM Prerequisites:
 REM     Bootless Star 1.5.4 or later
 REM         http://forum.drdteam.org/viewtopic.php?p=53914#p53914
+REM     Git Version Control CUI Toolset [OPTIONAL]
+REM         https://git-scm.com/
 REM     7Zip 9.20 or later
 REM         http://7-zip.org/
 REM     TGRDM3 [Morgenstern] resources [Local Working Copy]
+REM         https://github.com/SibTiger/TGRDM3
 REM ==========================================
 
 
